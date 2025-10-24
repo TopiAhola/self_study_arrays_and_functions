8a43ac7f7981e4ff3062745eb18fba64eb01c28d
Topi Ahola <topi.ahola@metropolia.fi>
2025-10-24 18:06:32 +0300

    Created
    -Topi yoga7

---
+++ pohja.js
@@ -0,0 +1,19 @@
+'use strict';
+let moduuli = "Arrays and functions";
+let tehtava = "Assignment ";
+let tehtavananto = `
+
+`
+
+
+
+
+
+
+const tuloste = "tulostetaan tämä";
+
+//Tulostetaan moduuli, tehtävän numero ja tuloste sivulle:
+document.querySelector(`#moduuli`).innerHTML = moduuli;
+document.querySelector('#tehtava').innerHTML = tehtava;
+document.querySelector('#tehtavananto').innerHTML = tehtavananto;
+document.querySelector('#tuloste').innerHTML = tuloste;
